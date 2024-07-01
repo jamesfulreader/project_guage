71a6b5892f802fa91aaa249be5d47247e1c38889
jamesfulreader <jamesfulreader@gmail.com>
2024-07-01 16:44:26 -0600

    create csv_upload page connect it to project_guage urls add data_upload to settings

--- data_upload/views.py
+++ data_upload/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def upload_csv(request):
+    return render(request, 'data_upload/csv_upload.html')
--- project_gauge/settings.py
+++ project_gauge/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'base.apps.BaseConfig'
+    'base',
+    'data_upload'
 ]
 
 MIDDLEWARE = [
