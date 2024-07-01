694f0750182aa4d75d43346f3268e3148180018b
jamesfulreader <jamesfulreader@gmail.com>
2024-07-01 16:59:45 -0600

    create models for tickets and file

--- data_upload/models.py
+++ data_upload/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Tickets(models.Model):
+    Resolved_By = models.CharField(max_length=100)
+    Status = models.CharField(max_length=50)
+    Parent_Record_Type = models.CharField(50)
+    Resolved_DateTime = models.DateTimeField()
+    Created_Date_Time = models.DateTimeField()
+
+
+class File(models.Model):
+    file = models.FileField(upload_to='csv')
