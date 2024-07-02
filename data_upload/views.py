83641e304ef466379d56cefc5b1794e63faa84c3
jamesfulreader <jamesfulreader@gmail.com>
2024-07-02 09:48:57 -0600

    add upload form and data display to html

--- data_upload/views.py
+++ data_upload/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .models import Tickets, File
+import pandas as pd
 
 # Create your views here.
 def upload_csv(request):
-    return render(request, 'data_upload/csv_upload.html')
+    data_to_display = None
+    if request.method == 'POST':
+        file = request.FILES['files']
+        obj = File.objects.create(file=file)
+        path = file.file
+        df = pd.read_csv(path)
+        data_to_display = df.to_html()
+    context = {'data_to_display': data_to_display}
+    return render(request, 'data_upload/csv_upload.html', context)
