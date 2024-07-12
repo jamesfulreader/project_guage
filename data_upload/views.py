f87156b9fe6844fb3609b081030ba0cc5d2b0ad0
jamesfulreader <jamesfulreader@gmail.com>
2024-07-12 13:08:20 -0600

    add basic validation

--- data_upload/views.py
+++ data_upload/views.py
@@ -14,29 +14,27 @@ def upload(request):
         if form.is_valid():
             file = form.cleaned_data['csv_file']
             obj = File.objects.create(file=file)
-            # path = os.path.join(settings.MEDIA_ROOT, 'uploads', file.name)
             path = obj.file.path
-            df = pd.read_csv(path)
-            data_to_display = df.head().to_html()
-            
-            for index, row in df.iterrows():
-                _, created = Tickets.objects.update_or_create(
+            file_extension = file.name.split('.')[-1].lower()
+            if file_extension == 'csv':
+                df = pd.read_csv(path)
+                data_to_display = df.head().to_html()
+                for index, row in df.iterrows():
+                    _, created = Tickets.objects.update_or_create(
                     Resolved_By = row['Resolved By'],
                     Status = row['Status'],
                     Parent_Record_Type = row['Parent Record Type'],
                     Resolved_DateTime = row['Resolved DateTime'],
                     Created_Date_Time = row['Created Date Time'],
                 )
-            
-            context = messages.success(request, 'Uploading your data now')
+                context = messages.success(request, 'Uploading your data now')
+            else:
+                context = messages.error(request, f'Error unsupported file extension: {file_extension}') 
         else:
             context = messages.error(request, 'Error Processing the form please verify it matches the format')
             print(form.errors.as_text())
-        return render(request, 'data_upload/upload.html', context)
+        return render(request, 'data_upload/upload.html')
     else:
         form = TicketUploadForm()
-    context = {
-            'data_to_display': data_to_display,
-            'form': form,
-               }
+    context = {'data_to_display': data_to_display}
     return render(request, 'data_upload/upload.html', context)
