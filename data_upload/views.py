9a2dd99981177872d7d5b5059481e1b5e5ef5144
jamesfulreader <jamesfulreader@gmail.com>
2024-07-16 11:00:15 -0600

    implement try catch to display error message rather than crash

--- data_upload/views.py
+++ data_upload/views.py
@@ -12,24 +12,28 @@ def upload(request):
     if request.method == 'POST':
         form = TicketUploadForm(request.POST, request.FILES)
         if form.is_valid():
-            file = form.cleaned_data['csv_file']
-            obj = File.objects.create(file=file)
-            path = obj.file.path
-            file_extension = file.name.split('.')[-1].lower()
-            if file_extension == 'csv':
-                df = pd.read_csv(path)
-                data_to_display = df.head().to_html()
-                for index, row in df.iterrows():
-                    _, created = Tickets.objects.update_or_create(
-                    Resolved_By = row['Resolved By'],
-                    Status = row['Status'],
-                    Parent_Record_Type = row['Parent Record Type'],
-                    Resolved_DateTime = row['Resolved DateTime'],
-                    Created_Date_Time = row['Created Date Time'],
-                )
-                context = messages.success(request, 'Uploading your data now')
-            else:
-                context = messages.error(request, f'Error unsupported file extension: {file_extension}') 
+            try:
+                file = form.cleaned_data['csv_file']
+                obj = File.objects.create(file=file)
+                path = obj.file.path
+                file_extension = file.name.split('.')[-1].lower()
+                if file_extension == 'csv':
+                    df = pd.read_csv(path)
+                    data_to_display = df.head().to_html()
+                    for index, row in df.iterrows():
+                        _, created = Tickets.objects.update_or_create(
+                        Resolved_By = row['Resolved By'],
+                        Status = row['Status'],
+                        Parent_Record_Type = row['Parent Record Type'],
+                        Resolved_DateTime = row['Resolved DateTime'],
+                        Created_Date_Time = row['Created Date Time'],
+                    )
+                    context = messages.success(request, 'Uploading your data now')
+                else:
+                    context = messages.error(request, f'Error unsupported file extension: {file_extension}')
+            except Exception as e:
+                context = messages.error(request, f'Error processing file {e}')
+                print(form.errors.as_text())
         else:
             context = messages.error(request, 'Error Processing the form please verify it matches the format')
             print(form.errors.as_text())
